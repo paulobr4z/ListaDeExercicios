99494bda9cfa4f70a6a09577fb07caa1e62783a4
Paulo Braz <paulobraztuc@gmail.com>
2019-07-09 21:52:26 -0300

    Update 004 - media.py

--- 001 - EstruturaSequencial/004 - media.py
+++ 001 - EstruturaSequencial/004 - media.py
@@ -1,11 +1,11 @@
 '''
 Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 '''
-n1 = int(input('Digite o 1 numero: '))
-n2 = int(input('Digite o 2 numero: '))
-n3 = int(input('Digite o 3 numero: '))
-n4 = int(input('Digite o 4 numero: '))
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+n4 = int(input('Digite o quarto numero: '))
 
 media = (n1 + n2 + n3 + n4) / 4
 
-print('A media é:', media)
+print(f'A media é: {media}')
