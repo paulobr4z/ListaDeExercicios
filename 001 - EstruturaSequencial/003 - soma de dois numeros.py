1b736eb183d30659cc0fc85bf567ca3bf0fe4d40
Paulo Braz <paulobraztuc@gmail.com>
2019-07-09 21:24:33 -0300

    Update 003 - soma de dois numeros.py

--- 001 - EstruturaSequencial/003 - soma de dois numeros.py
+++ 001 - EstruturaSequencial/003 - soma de dois numeros.py
@@ -4,4 +4,4 @@ Faça um Programa que peça dois números e imprima a soma.
 n1  = int(input('Digite o primeiro número: '))
 n2  = int(input('Digite o segundo número: '))
 soma  = n1 + n2
-print('Soma =', soma)
+print(f'Soma = {soma}')
