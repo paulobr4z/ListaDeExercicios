4e6eb111113a3b2030a170f685a0ecbc18a1c0e5
Paulo Braz <paulobraztuc@gmail.com>
2019-07-09 21:22:25 -0300

    Update 002 - Info numero.py

--- 001 - EstruturaSequencial/002 - Info numero.py
+++ 001 - EstruturaSequencial/002 - Info numero.py
@@ -3,4 +3,4 @@ Faça um Programa que peça um número e então
 mostre a mensagem O número informado foi [número].
 '''
 n  = int(input('Digite um número qualquer: '))
-print('O numero informado foi:', n)
+print(f'O numero informado foi: {n}')
