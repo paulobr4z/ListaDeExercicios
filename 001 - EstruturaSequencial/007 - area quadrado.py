f6c88eadfa1ad83512e1d1c5e8b2b27d45d56010
Paulo Braz <paulobraztuc@gmail.com>
2019-07-09 21:52:44 -0300

    Update 007 - area quadrado.py

--- 001 - EstruturaSequencial/007 - area quadrado.py
+++ 001 - EstruturaSequencial/007 - area quadrado.py
@@ -3,8 +3,6 @@ Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro
 desta área para o usuário.
 '''
 l = int(input('Qual o dado do quadrado? '))
-
-a = l*l
-
-print('A area do quadrado é:', a)
-print ('O dobro da area é:',a*a)
+a = l**2
+print(f'A area do quadrado é:{a}')
+print (f'O dobro da area é: {a*a}')
