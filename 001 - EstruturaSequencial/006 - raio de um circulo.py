d5c7ce52d2fbba580281d4fd0a99c0dd619ed4fa
Paulo Braz <paulobraztuc@gmail.com>
2019-07-09 21:52:40 -0300

    Update 006 - raio de um circulo.py

--- 001 - EstruturaSequencial/006 - raio de um circulo.py
+++ 001 - EstruturaSequencial/006 - raio de um circulo.py
@@ -1,11 +1,7 @@
 '''
 Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
 A = pi * r2
-
 '''
-r = float(input('Qual o raio? '))
-
-
+r = float(input('Qual o valor do raio? '))
 a = 3.14 *(r*r)
-
-print('A area do circulo é:', a)
+print(f'A area do circulo é: {a}')
