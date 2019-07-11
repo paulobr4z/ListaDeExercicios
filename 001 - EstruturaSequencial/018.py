99f0e4ff6f10dfcadd0bcad84ab8321f59aa869f
Paulo A. Braz de Araujo <52705622+paulobrazaraujo@users.noreply.github.com>
2019-07-11 14:36:04 -0300

    Update 018.py

--- 001 - EstruturaSequencial/018.py
+++ 001 - EstruturaSequencial/018.py
@@ -1,13 +1,11 @@
 '''
-Faça um programa que peça o tamanho de um arquivo para download (em MB) e a
-velocidade de um link de Internet (em Mbps), calcule e informe o tempo
+Faça um programa que peça o tamanho de um arquivo para download (em MB)
+e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo
 aproximado de download do arquivo usando este link (em minutos).
 '''
-tamanho_arquivo = 10#int(input('Qual o tamanho do arquivo: '))
-velocidade_internet = 1#int(input('Qual a velocidade da internet: '))
+tamanho = float(input('Informe o tamanho do arquivo (em MB): '))
+velocidade = float(input('Informe a velocidade de conexao (em Mbps): '))
 
-tempo_download = tamanho_arquivo * (velocidade_internet / 60)
+tempoDownload = (tamanho /(velocidade/8))/60
 
-print(f'{tempo_download // 60} minutos')
-print(f'{tempo_download / 60} segundos')
-print(f'{tempo_download:.2f}')
+print(f'Tempo aproximado de download: {tempoDownload:.0f} minutos.')
