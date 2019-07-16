42876ad36839844b57a6825b1b2a60ba4032ece1
Paulo A. Braz de Araujo <52705622+paulobrazaraujo@users.noreply.github.com>
2019-07-16 15:01:52 -0300

    Update 001.py

--- 005 - ExerciciosFuncoes/001.py
+++ 005 - ExerciciosFuncoes/001.py
@@ -1,3 +1,20 @@
+'''
+Faça um programa para imprimir:
+    1
+    2   2
+    3   3   3
+    .....
+    n   n   n   n   n   n  ... n
+'''
+#ex1
+def num(n):
+    for i in range(1,n+1):
+        print(str(i)*i)
+    return n
+
+num(n = int(input('N: ')))
+
+#ex2
 def num(n):
     lista = []
     for i in range(1,n+1):
