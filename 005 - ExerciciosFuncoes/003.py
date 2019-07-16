e0d67297bf07ce6a93b85302abbdec2433eec9b2
Paulo Braz <paulobraztuc@gmail.com>
2019-07-16 20:49:04 -0300

    Create 003.py

---
+++ 005 - ExerciciosFuncoes/003.py
@@ -0,0 +1,17 @@
+'''
+Faça um programa, com uma função que necessite de três argumentos, e que
+forneça a soma desses três argumentos.
+'''
+
+#ex1
+def soma(n1,n2,n3):
+    soma = n1+n2+n3
+    print(soma)
+
+#ex2
+def exercicio3(a, b, c):
+     return a + b + c
+    
+soma(1,2,3)
+print(exercicio3(4,5,6))
+
