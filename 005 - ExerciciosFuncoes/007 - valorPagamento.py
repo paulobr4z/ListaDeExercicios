011158c7eded05d6249ee86bf4f058611786f87c
Paulo A. Braz de Araujo <52705622+paulobrazaraujo@users.noreply.github.com>
2019-07-24 16:10:03 -0300

    Create 007 - valorPagamento.py

---
+++ 005 - ExerciciosFuncoes/007 - valorPagamento.py
@@ -0,0 +1,38 @@
+'''
+Faça um programa que use a função valorPagamento para determinar o valor a ser
+pago por uma prestação de uma conta. O programa deverá solicitar ao usuário o
+valor da prestação e o número de dias em atraso e passar estes valores para a
+função valorPagamento, que calculará o valor a ser pago e devolverá este valor
+ao programa que a chamou. O programa deverá então exibir o valor a ser pago na
+tela. Após a execução o programa deverá voltar a pedir outro valor de prestação
+e assim continuar até que seja informado um valor igual a zero para a prestação.
+Neste momento o programa deverá ser encerrado, exibindo o relatório do dia,
+que conterá a quantidade e o valor total de prestações pagas no dia. O cálculo
+do valor a ser pago é feito da seguinte forma. Para pagamentos sem atraso,
+cobrar o valor da prestação. Quando houver atraso, cobrar 3% de multa, mais
+0,1% de juros por dia de atraso.
+'''
+def valorPagamento():
+    saldo = 0
+    nPagamento = 0
+    valor = 9999
+
+    while valor != 0:
+        valor = int(input('Digite o valor da prestação: '))
+        if valor <= 0:
+            break
+        else:
+            nPagamento += 1
+        diaAtraso = int(input('Digite o número de dias de atraso: '))
+        if diaAtraso > 0:
+            multa = valor*0.03
+            juros = (valor+multa)*(diaAtraso*0.001)
+            total = valor+multa+juros
+            saldo += total
+            print(f'Valor da prestação R$ {total:.2f} reais')
+        else:
+            print(f'Valor da prestação R$ {valor} reais')
+    print(f'N° pagamentos: {nPagamento}')
+    print(f'Valor total: R$ {saldo:.2f} reais')
+
+valorPagamento()
