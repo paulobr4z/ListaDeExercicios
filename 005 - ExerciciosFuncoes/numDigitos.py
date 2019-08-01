52d611fd72e769504305c47afa978b1424a58c78
Paulo A. Braz de Araujo <52705622+paulobrazaraujo@users.noreply.github.com>
2019-08-01 11:16:10 -0300

    Create numDigitos.py

---
+++ 005 - ExerciciosFuncoes/numDigitos.py
@@ -0,0 +1,9 @@
+'''
+Faça uma função que informe a quantidade de dígitos de um determinado número
+inteiro informado.
+'''
+
+def numDigitos(num):
+    print(f'{num} tem {len(str(num))} digitos')
+    
+numDigitos(12345)
