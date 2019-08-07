bcdb6149001b6e3eef5cb3b78e49e967005fcda5
Paulo Braz <paulobraztuc@gmail.com>
2019-08-06 22:04:44 -0300

    Create 001.py

---
+++ 006 - ExerciciosComStrings/001.py
@@ -0,0 +1,5 @@
+'''
+Tamanho de strings. Faça um programa que leia 2 strings e informe o conteúdo
+delas seguido do seu comprimento. Informe também se as duas strings possuem o
+mesmo comprimento e são iguais ou diferentes no conteúdo.
+'''
